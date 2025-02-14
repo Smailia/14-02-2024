4adddbff8c86b0ccf43577a5d88ab2352db4a569
Omar Smailia <smailia.omar@iisgalvanimi.edu.it>
2025-02-14 12:01:26 +0000

    quarto commit

--- py.py
+++ py.py
@@ -1,2 +1,13 @@
-import pandas as pd 
+inventario = {
 
+0 : {"nome": "Il PC di ...", "quantità": 1, "prezzo": 3999},
+1 : {"nome": "Nintendo Switch 2", "quantità": 10, "prezzo": 449},
+2 : {"nome": "GTA VI", "quantità": 3, "prezzo": 100}
+} 
+
+while true:
+   scelta = input("Premi: 1. Aggiungere un prodotto nome, quantità e prezzo 2. Modificare la quantità di un prodotto esistente 3. Modificare il prezzo di un prodotto esistente 4. Eliminare un prodotto dall’inventario 5. Visualizzare l’intero inventario in modo leggibile")
+if scelta == 1:
+        x = str(input("Aggiungi nome: "))
+        y = str(input("Aggiungi quantità"))
+        z = str(input("Aggiungi prezzo"))
